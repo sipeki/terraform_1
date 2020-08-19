3f53250910795d2a90afc2e98d8ea31e3ff4c1c1
Ubuntu <ubuntu@ip-172-31-41-205.eu-west-1.compute.internal>
2020-08-19 11:43:23 +0000

    created VPC

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc" {
-    cidr_block = var.cidr_block
-    enable_dns_hostnames = true
-    
-    tags = {
-        Name = "VPC190820"
-    }
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "VPC190820"
+  }
 
 }
 
