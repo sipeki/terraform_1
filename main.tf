d0567dc92e6584fa571b04490805fa2bc12ff2ef
sipeki <sipeki@kindlen.eu>
2020-08-19 13:47:16 +0000

    routing table and inter gateway

--- main.tf
+++ main.tf
@@ -24,6 +24,30 @@ resource "aws_subnet" "subnet_a" {
 
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.sn_cidr_block
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.r.id
+}
+
 resource "aws_instance" "web" {
   ami                         = var.ami_id
   instance_type               = var.instance
@@ -31,4 +55,5 @@ resource "aws_instance" "web" {
   subnet_id                   = aws_subnet.subnet_a.id
   associate_public_ip_address = true
 }
+
 
