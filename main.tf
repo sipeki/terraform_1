d09e16ac04fab7c4bc200cbcb35cc4158ed77930
sipeki <sipeki@kindlen.eu>
2020-08-20 09:44:44 +0000

    after terraform innit

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region                  = var.region
   shared_credentials_file = var.cred
 }
+
+module "sipeki_vpc" {
+  source = "./VPC"
+
+}
+
+module "sipeki_instance" {
+  source = "./EC2"
+
+}
